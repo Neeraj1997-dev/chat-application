73198562f3d41b8045e6469351e7485b82c5eaf7
Kapable <tech@kapable.club>
2022-07-02 10:50:32 +0530

    chat add

---
+++ src/config.js
@@ -0,0 +1,5 @@
+export default {
+  appId: "213471be34c8d0e6", //Enter your App ID
+  apiKey: "309c6776796b5592a8284e365cac72fb18e6519b", //Enter your API KEY
+  GUID: "goloang", // Enter your group UID
+};
